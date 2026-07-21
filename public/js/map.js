3aa57d8140d4ad3660ed93ec19e3954bfd316df3
Alessio C <alessiococi1986@gmail.com>
2026-07-21 14:45:51 +0100

    Try color change with test data

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,28 @@
 const map = L.map("map").setView([53.4, -8.0], 7);
 
+const testData = {
+  GALWAY: 50,
+  MAYO: 38,
+  DONEGAL: 34,
+  DUBLIN: 8,
+};
+
+function getCountyColour(value) {
+  if (value === undefined) {
+    return "#cccccc";
+  }
+
+  if (value >= 40) {
+    return "#176b4d";
+  }
+
+  if (value >= 20) {
+    return "#70ad8b";
+  }
+
+  return "#d8eadf";
+}
+
 /*L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
@@ -7,11 +30,25 @@ const map = L.map("map").setView([53.4, -8.0], 7);
 }).addTo(map);*/
 
 function addCountyInteraction(feature, layer) {
+  //feature info about COUNTY, e.g. COUNTY: CORK - layer is county shape
   const countyName = feature.properties.COUNTY;
 
   layer.bindTooltip(countyName, {
+    //layer puts that name onto the visible map shape
     sticky: true,
   });
+}
+
+function styleCounty(feature) {
+  const countyName = feature.properties.COUNTY;
+  const value = testData[countyName];
+
+  return {
+    color: "#ffffff",
+    weight: 1,
+    fillColor: getCountyColour(value),
+    fillOpacity: 0.8,
+  };
 }
 
 async function loadCountyBoundaries() {
@@ -24,12 +61,7 @@ async function loadCountyBoundaries() {
   const geojson = await response.json();
 
   const countyLayer = L.geoJSON(geojson, {
-    style: {
-      color: "#ffffff",
-      weight: 0.5,
-      fillColor: "#2f7658",
-      fillOpacity: 0.35,
-    },
+    style: styleCounty,
     onEachFeature: addCountyInteraction,
   }).addTo(map);
 
